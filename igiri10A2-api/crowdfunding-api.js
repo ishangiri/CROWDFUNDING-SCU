c5ed79b956173d2c433969b8354041680fdc1a6d
ishangiri <ishansmith2@gmail.com>
2024-09-13 11:44:39 +1000

    Connection to database successful

--- igiri10A2-api/crowdfunding-api.js
+++ igiri10A2-api/crowdfunding-api.js
@@ -17,7 +17,7 @@ app.get('/api/fundraisers', (req, res) => {
   });
 });
 
-// ... rest of your routes ...
 
-const PORT = process.env.PORT || 3000;
+
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
